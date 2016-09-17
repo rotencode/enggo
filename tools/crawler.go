b133d2c4d9f5847f00520158c5c671e00d91ebc9
songtzu <songtzu>
2016-09-17 16:48:40 +0800

    crawle data from google

--- tools/crawler.go
+++ tools/crawler.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"sort"
 	"strconv"
 
 	"../model"
@@ -33,9 +34,20 @@ type Crawler struct {
 }
 
 func (crawler *Crawler) CrawlerRegexMatch(str string) (hasNext bool) {
+	//	fmt.Println(str)
+	//	defer recover()
+
 	hasNext = false
-	begin := strings.Index(str, "<td  class=\"lm\">")
+	begin := strings.Index(str, "class=\"lm\">")
+	if begin == -1 {
+		//		fmt.Println(str)
+		return false
+	}
 	end := strings.Index(str[begin:], "</table>")
+	if end == -1 {
+		//		fmt.Println(str)
+		return false
+	}
 	pure := str[begin : begin+end]
 
 	items := strings.Split(pure, "<tr>")
@@ -43,11 +55,16 @@ func (crawler *Crawler) CrawlerRegexMatch(str string) (hasNext bool) {
 		//		fmt.Println("....", item, "---")
 		//r, _ := regexp.Compile(".+<td  class=\"lm\">([d-]+)<td  class=\"rgt\">([d\\.]+)<td  class=\"rgt\">([d\\.]+)<td  class=\"rgt\">([d\\.]+)<td  class=\"rgt\">([d\\.]+)")
 		//r, _ := regexp.Compile(".+<td  class=\"lm\">([\\d-]+)")
-		r, _ := regexp.Compile("<td  class=\"lm\">([\\d-]+)[\\W]+<td  class=\"rgt\">([\\d.]+)[\\W]+<td  class=\"rgt\">([\\d.]+)[\\W]+<td  class=\"rgt\">([\\d.]+)[\\W]+<td  class=\"rgt\">([\\d.]+)[\\W]+<td  class=\"rgt rm\">([\\d,]+)")
+		r, _ := regexp.Compile("class=\"lm\">([\\d-]+)[\\W]+<td class=\"rgt\">([\\d.]+)[\\W]+<td class=\"rgt\">([\\d.]+)[\\W]+<td class=\"rgt\">([\\d.]+)[\\W]+<td class=\"rgt\">([\\d.]+)[\\W]+<td class=\"rgt rm\">([\\d*,]+)")
 		arr := r.FindStringSubmatch(item)
-		fmt.Println(arr[1], arr[2], ";", arr[3], ";", arr[4], ";", arr[5], ";", arr[6])
-
+		fmt.Println("==========>")
+		fmt.Println(arr)
+		fmt.Println("《==========")
 		if len(arr) == 7 {
+
+			fmt.Println(arr[1], arr[2], ";", arr[3], ";", arr[4], ";", arr[5], ";", arr[6])
+
+			//			fmt.Println("....", item, "---")
 			// 日期/開市價/最高價/最低價/收市價/成交量
 			var exchange model.ExchangeData
 			exchange.ExchangeDate = arr[1]
@@ -59,8 +76,13 @@ func (crawler *Crawler) CrawlerRegexMatch(str string) (hasNext bool) {
 			//
 			fmt.Println("==exchange===>", exchange)
 			crawler.cache[exchange.ExchangeDate] = exchange
+		} else {
+			fmt.Println("....", item, "---")
+			fmt.Println("错误数据")
 		}
+
 	}
+	fmt.Print("内容的长度%d\n\n", len(items))
 	if len(items) == 200 {
 		hasNext = true
 	}
@@ -76,17 +98,32 @@ func (crawler *Crawler) getUrl() (url string, err error) {
 	return
 }
 func (crawler *Crawler) crawlerSave() {
-	ofile := string("./data/" + string(crawler.Market))
+	fmt.Println("crawlerSave", crawler.cache)
+	ofile := string("./data/" + string(crawler.Stockid))
 	fout, err := os.Create(ofile)
 	defer fout.Close()
 	if err == nil {
-		for k, v := range crawler.cache {
-			fmt.Printf("k=%v, v=%v\n", k, v)
-			fout.WriteString(fmt.Sprintln("%v", v))
 
+		var keys []string
+		for k := range crawler.cache {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+
+		for _, k := range keys {
+			//			fmt.Println(crawler.cache[k].ExchangeDate, " ", crawler.cache[k].PriceFirst, " ", crawler.cache[k].PriceHigh, " ", crawler.cache[k].PriceLow, " ", crawler.cache[k].PriceLast, " ", crawler.cache[k].ExchangeAmount)
+			//			fmt.Printf("k=%v, v=%v\n", k, v)
+			str := fmt.Sprintf("%s %.3f %.3f %.3f %.3f %d\n", crawler.cache[k].ExchangeDate, crawler.cache[k].PriceFirst, crawler.cache[k].PriceHigh, crawler.cache[k].PriceLow, crawler.cache[k].PriceLast, crawler.cache[k].ExchangeAmount)
+			fmt.Println(str)
+			fout.WriteString(str)
+			//			fout.WriteString(fmt.Sprintln("",crawler.cache[k].ExchangeDate, " ", crawler.cache[k].PriceFirst, " ", crawler.cache[k].PriceHigh, " ", crawler.cache[k].PriceLow, , " ", crawler.cache[k].PriceLast, " ", crawler.cache[k].ExchangeAmount))
+			//			fout.WriteString(fmt.Sprintln(crawler.cache[k].ExchangeDate, " ", crawler.cache[k].PriceFirst, " ", crawler.cache[k].PriceHigh, " ", crawler.cache[k].PriceLow, , " ", crawler.cache[k].PriceLast, " ", crawler.cache[k].ExchangeAmount)
 		}
 		fmt.Sprintln("%v")
+	} else {
+		fmt.Print("错误", err)
 	}
+
 }
 func (crawler *Crawler) CrawlerRequest() (str string, err error) {
 	c_url, _ := crawler.getUrl()
@@ -109,6 +146,7 @@ func (crawler *Crawler) CrawlerRequest() (str string, err error) {
 	fmt.Println("task begin")
 
 	fmt.Println(body)
+	crawler.position += 200
 	return
 }
 func (crawler *Crawler) CrawlerInitCache() {
@@ -123,7 +161,9 @@ func (crawler *Crawler) CrawlerTask() {
 		if err == nil {
 			hasNext := crawler.CrawlerRegexMatch(str)
 			if !hasNext {
+				fmt.Println("保存到数据文件中")
 				crawler.crawlerSave()
+				return
 			}
 		}
 	}
